0614dac705320e5cfbc59e3be9dcb6d2476a5887
slyjay <duahsylvester24@gmail.com>
2025-03-18 22:38:35 -0500

    main_program.py

--- graph/main_program.py
+++ graph/main_program.py
@@ -151,6 +151,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# set up the connection for the arangodb
 def setup_arangodb_connection(db_name="graph_db", graph_name="ai_incidents_graph"):
     """
     Set up the ArangoDB connection and create the graph and collections if they don't exist.
